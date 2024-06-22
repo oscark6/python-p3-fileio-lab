42782eb835e65f2d0fb1eb54fb33a1ac8e3aac9c
oscark6 <oscar.kimani1@student.moringaschool.com>
2024-06-22 12:32:24 +0300

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,24 @@
-def write_file(file_name, file_content):
-    pass
+def write_file(file_name: str, file_content: str):
+    """Write file_content to file_name with.txt extension."""
+    with open(f"{file_name}.txt", "w") as f:
+        f.write(file_content)
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name: str, file_content: str):
+    """Append file_content to file_name with.txt extension."""
+    with open(f"{file_name}.txt", "a") as f:
+        f.write(file_content)
 
-def read_file(file_name):
-    pass
+def read_file(file_name: str):
+    """Read file_name with.txt extension and return content."""
+    with open(f"{file_name}.txt", "r") as f:
+        file_content = f.read()
+    return file_content
+
+# Example usage
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added")
+append_file(file_name="logfile", file_content="Log 2: 3 bananas subtracted")
+
+print(read_file(file_name="logfile"))
+# Output:
+# Log 1: 5 bananas added
+# Log 2: 3 bananas subtracted
